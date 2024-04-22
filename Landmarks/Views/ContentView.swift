f9227ba2595434953e81768b8f60bc8b73e735ac
Behzad Karimi <karimi1993@live.de>
2024-04-22 15:51:02 +0200

    building lists and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Behzad Karimi on 22.04.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
